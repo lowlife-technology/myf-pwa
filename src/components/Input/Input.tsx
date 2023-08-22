4b4c435990c4365ca7314ef91209884a42bc1a02
otherRick <drckbr@gmail.com>
2023-08-22 14:08:46 -0300

    feat/ create register flow done

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { FieldValues, UseFormReturn } from 'react-hook-form';
 
 interface InputProps {
-  onInputButton?: () => void;
+  onInputButton: () => void;
   inputButton?: boolean;
   label?: string;
   form: UseFormReturn<FieldValues>;
@@ -29,10 +29,11 @@ export const Input = ({
   return (
     <div className='w-full gap-2 flex flex-col'>
       <Text>{label}</Text>
-      <div className='flex h-11 p-1 items-center rounded-full w-full shadow-inner justify-between '>
+      <div className='flex h-11 p-1  items-center rounded-full w-full shadow-inner justify-between '>
         <input
           {...form.register(name)}
           type={inputType}
+          autoComplete={inputType === 'password' ? 'on' : ''}
           placeholder={placeholder}
           style={{ paddingLeft: '12px' }}
           className='bg-transparent focus:outline-none opacity-60 text-grey-3 text-sm w-full placeholder-grey-2'
@@ -41,10 +42,10 @@ export const Input = ({
           <button
             type={inputButtonType}
             onClick={() => {
-              onInputButton;
+              onInputButton();
               setIsBouncing(!isBouncing);
             }}
-            className={`bg-brand-grey1 items-center flex justify-center rounded-full w-8 h-8 shadow-button mr-1 cursor-pointer ${
+            className={`bg-brand-grey1 items-center  flex justify-center rounded-full w-8 h-8 shadow-button mr-1 cursor-pointer ${
               isBouncing ? 'animate-pushPull' : ''
             }`}
           >
--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function PrivateRoutes() {
   const Home = lazy(() => import('../pages/Home/Home'));
-  const PhoneEmail = lazy(() => import('../pages/Register/PhoneEmail/PhoneEmail'));
+  const Register = lazy(() => import('../pages/Register/Registe'));
   return (
     <Suspense
       fallback={
@@ -15,7 +15,7 @@ export default function PrivateRoutes() {
       <BrowserRouter>
         <Routes>
           <Route path='/' Component={Home} />
-          <Route path='/register' Component={PhoneEmail} />
+          <Route path='/register' Component={Register} />
         </Routes>
       </BrowserRouter>
     </Suspense>
